42ad64631b7ee3fa8e7c6f6cb0946ce0b6a9e123
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-12-23 01:51:15 -0800

    Add files via upload

---
+++ os_reviveStationV2/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'OnlyScripts / VeryAppropriateName#2324 | veryappropriatename'
+version '2.0.0'
+
+client_scripts {
+	"client.lua"
+}
+
+server_scripts {
+    "server.lua"
+}
---
+++ os_reviveStationV2/server.lua
@@ -0,0 +1,21 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+function CountAmbulanceJob()
+  local count = 0
+  for _, playerId in ipairs(GetPlayers()) do
+    local xPlayer = ESX.GetPlayerFromId(playerId)
+
+    if xPlayer.job.name == 'ambulance' then
+      count = count + 1
+    end
+  end
+  return count
+end
+
+RegisterServerEvent('os_revivestation:client:count')
+AddEventHandler('os_revivestation:client:count', function()
+  local count = CountAmbulanceJob()
+  TriggerClientEvent('os_revivestation:client:receiveCount', source, count)
+end)
