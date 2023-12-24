1105811979ef83d1211afdb3d7f31d615cb4b3ce
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-12-24 14:18:16 -0800

    Update server.lua

--- os_reviveStationV2/server.lua
+++ os_reviveStationV2/server.lua
@@ -6,14 +6,14 @@ function CountAmbulanceJob()
   local count = 0
   for _, playerId in ipairs(GetPlayers()) do
     local xPlayer = ESX.GetPlayerFromId(playerId)
-
-    if xPlayer.job.name == 'ambulance' then
-      count = count + 1
+    if xPlayer ~= nil then
+      if xPlayer.job.name == 'ambulance' then
+        count = count + 1
+      end
     end
   end
   return count
 end
-
 RegisterServerEvent('os_revivestation:client:count')
 AddEventHandler('os_revivestation:client:count', function()
   local count = CountAmbulanceJob()
