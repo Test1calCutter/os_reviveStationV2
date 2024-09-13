fdc14993bc3578adb1803b4d1941f04a7eec934f
Noel <108223454+Test1calCutter@users.noreply.github.com>
2024-09-13 03:36:33 +0200

    Update fxmanifest.lua

--- os_reviveStationV2/fxmanifest.lua
+++ os_reviveStationV2/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 author 'OnlyScripts / VeryAppropriateName#2324 | veryappropriatename'
-version '2.0.0'
+version '2.1.0'
 
 client_scripts {
 	"client.lua"
